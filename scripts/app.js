e932fd148c1d07af80db15c51821d7a494daf2f8
Qiu Juntao <juntao.qiu@gmail.com>
2015-07-14 00:15:04 +1000

    add carousel

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,12 @@
 $(document).ready(function() {
 	new WOW().init();
+	 $("#teams").owlCarousel({
+ 
+      autoPlay: 3000,
+ 
+      items : 4,
+      itemsDesktop : [1199,3],
+      itemsDesktopSmall : [979,3]
+ 
+  });
 });
